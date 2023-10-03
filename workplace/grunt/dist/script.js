1e5423463cd6e72ebb8649ac41d20da5b4d6e87e
suryashanm <shanmsurya@gmail.com>
2023-10-03 21:13:18 +0530

    grunt test

---
+++ workplace/grunt/dist/script.js
@@ -0,0 +1,10 @@
+/*Processed by SURYA on 3/10/2023@20:45:10*/
+function changeText() {
+  document.getElementById("text").innerHTML = "Script changed by JS";
+}
+
+function reload() {
+  location.reload();
+}
+
+//# sourceMappingURL=script.js.map
